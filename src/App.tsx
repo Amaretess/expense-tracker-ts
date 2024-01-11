f8e298cc50c6849ddfb70ac14127db4724e5f72e
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-10 22:08:24 -0800

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,13 @@ const App = () => {
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
+  console.log(watch("example"));
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input defaultValue="test" {...register("example")} placeholder="First Name" />
+      <input defaultValue="test" {...register("exampleRequired", { required: true })} placeholder="First Name" />
+      {errors.exampleRequired && <span>This field is required</span>}
       <input placeholder="Last Name" type='submit' />
       <input />
 
