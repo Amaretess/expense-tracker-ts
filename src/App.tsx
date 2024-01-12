bdcd02f1bb79f0fd695b4c5809b227fd1abf407f
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-11 20:31:22 -0800

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,13 @@ const App = () => {
       <Title>React Hook Form</Title>
       <Form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('firstName', { required: true, minLength: 3 })} type="text" placeholder="First name" />
-        {errors.firstName?.type === 'required' && <p>First name is required</p>}
+        {errors.firstName?.type === 'required' && <p className="text-danger" >First name is required</p>}
+        {errors.firstName?.type === 'minLength' && <p className="text-danger" >Need at least 3 characters</p>}
         <input {...register('lastName', { required: true, minLength: 3 })} type="text" placeholder="Last name" />
-        {errors.lastName?.type === 'required' && <p>Last name is required</p>}
+        {errors.lastName?.type === 'required' && <p className="text-danger" >Last name is required</p>}
+        {errors.lastName?.type === 'minLength' && <p className="text-danger" >Need at least 3 characters</p>}
         <input type="submit" />
       </Form>
-      <h1>DATA</h1>
     </Container>
   )
 }
@@ -32,9 +33,12 @@ const Title = styled.h1`
 
 const Container = styled.div`
   display: flex;
+  background-color: #242424;
   width: 100vw;
+  height: 100vh;
   flex-direction: column;
   align-items: center;
+  justify-content: flex-start;
 `
 
 const Form = styled.form`
