335173fc502d3a179fb44c40a14c0fecf48fe0de
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-11 18:03:52 -0800

    solidifying

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { useForm } from 'react-hook-form'
+import { FieldValues, useForm } from "react-hook-form"
+import styled from "styled-components";
+
 
 const App = () => {
 
-  const { register } = useForm()
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   return (
-    <>
-      <form onSubmit={(data) => console.log(data)}>
-        <input {...register('name')} type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
+    <Container>
+      <Title>React Hook Form</Title>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName')} type="text" placeholder="First name" />
+        <input {...register('lastName')} type="text" placeholder="Last name" />
         <input type="submit" />
       </form>
-    </>
+      <h1>DATA</h1>
+    </Container>
   )
 }
 
-export default App;
+
+export default App
+
+const Title = styled.h1`
+  color: #ec5891;
+`
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  flex-direction: column;
+  align-items: center;
+`
