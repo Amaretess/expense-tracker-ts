0fa848309e8a2286cad5cd784c024edddab0f288
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-11 21:05:37 -0800

    zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { FieldValues, useForm } from "react-hook-form"
 import styled from "styled-components";
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+const schema = z.object({
+  firstName: z.string().min(3, { message: "First name must be at least 3 characters" }),
+  lastName: z.string().min(3, { message: "Last name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "age must be of at least 21 years" })
+})
 
-interface FormData {
-  firstName: string;
-  lastName: string;
-}
+type FormData = z.infer<typeof schema>;
 
 const App = () => {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data)
 
@@ -17,12 +23,12 @@ const App = () => {
     <Container>
       <Title>React Hook Form</Title>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('firstName', { required: true, minLength: 3 })} type="text" placeholder="First name" />
-        {errors.firstName?.type === 'required' && <p className="text-danger" >First name is required</p>}
-        {errors.firstName?.type === 'minLength' && <p className="text-danger" >Need at least 3 characters</p>}
-        <input {...register('lastName', { required: true, minLength: 3 })} type="text" placeholder="Last name" />
-        {errors.lastName?.type === 'required' && <p className="text-danger" >Last name is required</p>}
-        {errors.lastName?.type === 'minLength' && <p className="text-danger" >Need at least 3 characters</p>}
+        <input {...register('firstName')} type="text" placeholder="First name" className="form-control" />
+        {errors.firstName && <p className="text-danger" >{errors.firstName.message}</p>}
+        <input {...register('lastName')} type="text" placeholder="Last name" className="form-control" />
+        {errors.lastName && <p className="text-danger" >{errors.lastName.message}</p>}
+        <input {...register('age', { valueAsNumber: true })} type="number" placeholder="age" className="form-control" />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
         <input type="submit" />
       </Form>
     </Container>
