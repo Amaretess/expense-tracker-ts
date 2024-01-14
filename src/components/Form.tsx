aa17d53efdea16ade773b01ab4f0c4597eb29bdc
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-13 21:34:29 -0800

    validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,22 @@
 import styled from "styled-components"
 import { useForm, FieldValues } from 'react-hook-form';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
+
+const schema = z.object({
+    description: z.string().min(3, { message: "Description needs at least 3 characters" }),
+    amount: z
+        .number({ invalid_type_error: "Amount is required" })
+        .min(1, { message: "amount must be at least $1" }),
+    category: z.string()
+
+})
+
+type FormData = z.infer<typeof schema>
 
 const Form = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({ resolver: zodResolver(schema) });
 
     const onSubmit = (data: FieldValues) => {
         console.log(data)
@@ -15,16 +28,24 @@ const Form = () => {
             <FormContainer onSubmit={handleSubmit(onSubmit)} >
                 <label htmlFor="description">Description</label>
                 <Input {...register('description')} type="text" name="description" />
+                {errors.description && <p className="text-danger" >{errors.description.message}</p>}
                 <label htmlFor="amount" >Amount</label>
                 <Input {...register('amount')} type="number" name="amount" />
+                {errors.amount && <p className="text-danger" >{errors.amount.message}</p>}
+
                 <label htmlFor="category">Category</label>
                 <Select {...register('category')} name="category" >
                     <option>Groceries</option>
                     <option>Utilities</option>
                     <option>Entertainment</option>
                 </Select>
-                <Input type="submit" className="btn btn-primary" />
+                <Button disabled={!isValid} type="submit" className="btn btn-primary" >Submit</Button>
             </FormContainer>
+            <select>
+                <option>Groceries</option>
+                <option>Utilities</option>
+                <option>Entertainment</option>
+            </select>
         </Container>
     )
 }
@@ -48,4 +69,8 @@ const Input = styled.input`
 
 const Select = styled.select`
     padding: .5rem;
+`
+
+const Button = styled.button`
+    margin: 1rem;
 `
