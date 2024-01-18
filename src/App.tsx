8c859df1737d3f2b85db7522dd9568786b8c62d3
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-17 20:07:23 -0800

    dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import ExpenseList from "./components/ExpenseList.tsx"
 import Form from "./components/Form.tsx"
 
 const App = () => {
+
+  const expenses = [
+    { id: 0, description: 'milk', amount: 5, category: 'groceries' },
+    { id: 1, description: 'ham', amount: 6, category: 'groceries' },
+    { id: 2, description: 'insurance', amount: 100, category: 'utilities' },
+  ]
+
   return (
     <>
       <Form />
+      <ExpenseList expenses={expenses} onDelete={(id) => console.log('delete', id)} />
     </>
   )
 }
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+interface Expense {
+    id: number;
+    description: string;
+    amount: number;
+    category: string;
+}
 
-const ExpenseList = () => {
+interface Props {
+    expenses: Expense[];
+    onDelete: (id: number) => void;
+}
+
+// reducer func => acc is the variable containing total
+
+// passing each expense as an object and shaping each expense
+const ExpenseList = ({ expenses, onDelete }: Props) => {
     return (
         <table className="table" >
             <thead>
@@ -8,15 +21,30 @@ const ExpenseList = () => {
                     <th>Description</th>
                     <th>Amount</th>
                     <th>Category</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>item 1</td>
-                    <td>$</td>
-                    <td>Groceries</td>
-                </tr>
+                {expenses.map(expense => <tr key={expense.id}>
+                    <td>{expense.description}</td>
+                    <td>{expense.amount}</td>
+                    <td>{expense.category}</td>
+                    <td>
+                        <button
+                            className="btn btn-outline-danger"
+                            onClick={() => onDelete(expense.id)}
+                        >Delete</button>
+                    </td>
+                </tr>)}
             </tbody>
+            <tfoot>
+                <tr>
+                    <td>Total</td>
+                    <td>${expenses.reduce((acc, expense) => expense.amount + acc, 0).toFixed(2)}</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tfoot>
 
         </table>
     )
