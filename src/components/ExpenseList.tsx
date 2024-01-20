8607f49b4d0f1a7800f6e6131d71e4125d0a4d7e
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-19 23:15:59 -0800

    editing header

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -17,7 +17,14 @@ const ExpenseList = ({ expenses, onDelete }: Props) => {
 
     return (
         <table className="table">
-
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th>placeholder</th>
+                </tr>
+            </thead>
             <tbody>
                 {expenses.map(expense => <tr key={expense.id}>
                     <td>{expense.description}</td>
