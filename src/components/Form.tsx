268f3ec6056806128b98b9dbb2106cd707a6e93c
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-19 23:17:47 -0800

    zod schema

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,14 +3,7 @@ import { useForm, FieldValues } from 'react-hook-form';
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 
-const schema = z.object({
-    description: z.string().min(3, { message: "Description needs at least 3 characters" }),
-    amount: z
-        .number({ invalid_type_error: "Amount is required" })
-        .min(1, { message: "amount must be at least $1" }),
-    category: z.string().min(1, { message: "Category is required" })
 
-})
 
 type FormData = z.infer<typeof schema>
 
